745206ba3f4926bb194606c3d3379b85151536d8
duende14 <84480089+duende14@users.noreply.github.com>
2023-01-17 22:20:39 -0300

    Add files via upload

---
+++ Tp-final/javascript/funciones.js
@@ -0,0 +1,29 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+function myFunct(){
+    location.href = '/html/about.html'
+}
+
+function myFunc(){
+    location.href = '/html/contact.html'
+}
+
+function myFu(){
+    location.href = '/html/home.html'
+    
+}
+
+function myFun(){
+    location.href = '/html/news.html'
+}
+function myFuncti(){
+    location.href = '/html/carrito.html'
+}
+
